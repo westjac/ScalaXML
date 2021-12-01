f09cc471ea15d54e7600effdf68ba1e7be7e1f9a
Jacob West <jwest@omnitech-inc.com>
2021-11-30 22:37:34 -0700

    Added the checklist to the mainStudentStarter

--- src/west_jacob/MainStarterStudent.scala
+++ src/west_jacob/MainStarterStudent.scala
@@ -1,5 +1,63 @@
-package west_jacob
+//    SCALA CHECKLIST
+//    0. Got it running						          X
+//
+//    1.	  Add + Display*	34
+//    Prompts correct (-1 pt each missed)		X
+//    Adds a one of each item and displays 	X
+//    Adds multiples 							          X
+//    Above displays correctly formatted 		X
+//
+//
+//    2A) Remove + Display*	10
+//    Prompts correct							          X
+//    Removes and displays correctly 			  X
+//
+//
+//    2B) Add + XML save*	16
+//    Console added items saved correctly 		    X
+//    Console added multiples is saved correctly 	X
+//
+//
+//    2C) XML load + XML save*	16
+//    1 XML file loaded and saved correctly 	      X
+//      2+ XML file loaded and saved correctly	    X
+//
+//
+//    2D) XML load + Display*	16
+//    1 XML file loaded and displays correctly 	  X
+//    2+ XML file loaded and displays correctly	  X
+//
+//
+//    2E) XML+ Display with bad file handing	10
+//    Calling display on empty, and after “messy” load works	X
+//    Handles files not found correctly						            X
+//      Handle “not taxonomy” correctly							          X
+//      Handles extra tag, text, and attributes correctly		  X
+//      Handles missing attributes correctly					        X
+//
+//
+//    3.	  Stress test for above*	15
+//    Loads in file, adds data, and displays/saves correctly		X
+//    Appends a file and displays/saves correctly 				      X
+//    Removes animal after edits, and displays/saves correctly 	X
+//
+//
+//    4. Find feature*	16
+//    CoR format at least there						            X
+//    Prompts correct									                X
+//    First item found and output formatted correctly	______/////////////////////////
+//    Handles “not found case”						            X
+//
+//
+//    5.	  Total species count*	12
+//    Prompts correct				X
+//    Calculated correctly	X
+//    Parallelized*				  X
+//
+//    Every Line with a * has its grading tag: ______//////////////////////////////
 
+
+package west_jacob
 import scala.io.StdIn
 
 
